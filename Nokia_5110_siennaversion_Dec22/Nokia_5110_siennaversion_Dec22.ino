e3589c3662435f505a3adc3cc6e9f4311cd39fea
Jimmy Jimmy Jimmy <kestrel@gmail.com>
2021-01-06 04:02:47 -0500

    Clean-up from today's bloodbath.
    Cosmetic tweaks on power-up.
    Tidying the code up.
    Removing placeholder values.
    Adding unit conversion.

--- Nokia_5110_siennaversion_Dec22/Nokia_5110_siennaversion_Dec22.ino
+++ Nokia_5110_siennaversion_Dec22/Nokia_5110_siennaversion_Dec22.ino
@@ -2,9 +2,9 @@
   Laser Receiver Code
   Hamming encoder/decoder and Optical modulator/demodulator are based on
   the LumenWire library written by Andrew Ramanjooloo and modified by
-  Tom Gitlin 
+  Tom Gitlin
   https://github.com/HobbyTransform/Encoded-Laser-and-LED-Serial-Communication
-  Arduino sketch by Tom Gitlin; modified slightly by J. Acevedo
+  Arduino sketch by Tom Gitlin; modified by Jimmy Acevedo.
   version 1.3 22 Dec 2020 -- Modifying to accommodate Nokia 5110 display.
 
   This sketch is expecting:
@@ -16,82 +16,90 @@
   5                     5, via 10k R
   6                     11, via 10k R
   7                     13, via 10k R
-  8                     (optional) 2.4-5V 
-  
-***************************************************************************/
-#include "Nokia_LCD_functions.h" //include library to drive NOKIA display
+  8                     (optional) 2.4-5V via 330 R
+
+  *** IMPORTANT: The Delay function is used sparingly because it stalls program execution
+  (except for interrupts) and can affect the ability to check if characters are ready. ***
+
+*************************************************************************** */
+#include "Nokia_LCD_functions.h"  //include library to drive NOKIA display
 #include <HammingEncDec.h>        // include the Hamming encoder/decoder functionality
 #include <OpticalModDemod.h>      // include the modulator/demodulator functionality
 
-// new packet structure by Dan "Memes" Koris
+/* --------- PRE-SETUP ---------- */
+
+/* new packet structure by Dan "Memes" Koris - m241dan */
 typedef union packet
 {
-    char bytes[10];
-    struct 
-    {
-        byte  starter      = '\x01';
-        float temperature  = 0.0f;
-        float humidity     = 0.0f;
-        byte  terminator   = '\x04';
-    }; 
+  char bytes[10];
+  struct
+  {
+    byte  starter      = '\x01';
+    float temperature  = 0.0f;
+    float humidity     = 0.0f;
+    byte  terminator   = '\x04';
+  };
 };
 
+int            linktimeout      = 1000; // if no valid characters received in this time period, assume the link is bad
+static int     MODULATE_SPEED   = 2000; // laser modulation speed - should be 500+ bits/second, nominal 2000 (=2KHz)
+                                        // MUST MATCH THE SPEED AS DEFINED IN THE OPTICALTRANSMIT CODE
+const int      PHOTOTRANSISTOR_RECEIVE = 2; // Pin value for the phototransistor input
+byte           c;                       // variable to hold the byte returned from the receiver
+unsigned long  timelastchar;            // variable to hold the time the last valid character was received
+bool           linkgood;                // flag to store optical link status
+bool           displayChanged;          // checks to clear screen if display has changed
+unsigned long  timenow;                 // holds the Arduino running time in milliseconds for display times
+int            receive_count    = 0;    // global to count received packets
+union          packet pkt;              // global receive buffer
+bool           valid_read = false;      // global to indicate the quality of our data.
+String         strTemper, strHumidi;    // holds the values of the measurands
+float          temperature_F = 0.0f;    // variable for a Fahrenheit conversion
+String         strTemperFahren;         // string for a Fahrenheit conversion
 
-int            linktimeout = 1000;    // if no valid characters received in this time period, assume the link is bad
-int            displayperiod = 2500;  // how long to keep a number displayed (in ms, 1000 ms = 1 second)
-int            laserreceivespeed = 2000; //MUST MATCH THE SPEED AS DEFINED IN THE OPTICALTRANSMIT CODE
-
-byte           c;                     // variable to hold the byte returned from the receiver
-int            i;                     // loop counter
-int            j;                     // alternate loop counter for button-press (going to pull it off the cyclic timer and replace with button)
-int            measurand;             // tracks the number of measurands (3 for the demo, Temp, Press and Humidity)
-unsigned long  timelastchar;          // variable to hold the time the last valid character was received
-bool           linkgood;              // flag to store optical link status
-bool           timetoswitch;          // flag to cycle through the measurands
-bool           blankedvalues;         // flag to determine if values have been blanked for a bad laser link
-unsigned long  timenow;               // holds the Arduino running time in milliseconds for display times
-bool           displayChanged;         //checks to clear screen if display has changed
-int            receive_count = 0; // global
-union          packet pkt; // global receive buffer
-bool           valid_read = false; // global to indicate the quality of our data.
-
-                                      
-/* IMPORTANT: The Delay function is used sparingly because it stalls program execution
-(except for interrupts) and can affects the ability to check if characters are ready. */
-                                         
-String         parameterValue;      // holds the measurand being built up character-by-character
-String         strTemperature, strPressure, strHumidity, displayTemp, displayHumid; // holds the values of the measurands
-char           tempChar;
-char           charsTemp[10], charsHumid[10];
-// DECONFLICT THESE PINS LATER -- they walk over pins reserved in the header file.
-//const int      LED_Temp = 6, LED_Press = 5, LED_Humid = 4;  // Pins for the Temp, Press, and Humidity LEDs
-const int      BUTTON_INPUT = 3; //Pin for button press input.
-
-// Pin value for the phototransistor input
-const int      PHOTOTRANSISTOR_RECEIVE = 2;
-const int      DISPLAY_NUMBER_OF_DISPLAYS = 1;
+const unsigned char nasa_worm_BMP [] = {
+0x00, 0xFC, 0xFE, 0xFF, 0xFF, 0x07, 0x0F, 0x1F, 0xFE, 0xFC, 0xF8, 0xC0, 0x00, 0x00, 0x00, 0x00,
+0x00, 0xFE, 0xFE, 0xFE, 0xFE, 0x00, 0x00, 0x00, 0x00, 0xE0, 0xFC, 0xFE, 0xFE, 0x1F, 0x0F, 0x0F,
+0x7E, 0xFE, 0xFC, 0xF0, 0x80, 0x00, 0x00, 0xE0, 0xF8, 0xFC, 0xFE, 0x9E, 0x0E, 0x0E, 0x0E, 0x0E,
+0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x80, 0xF0, 0xFC, 0xFE,
+0x7E, 0x1E, 0x0E, 0x1E, 0xFE, 0xFC, 0xF8, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF,
+0xFF, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x7F, 0xFF, 0xFC, 0xE0, 0x80, 0x00, 0x80, 0xFF, 0xFF, 0xFF,
+0xFF, 0x00, 0xC0, 0xF8, 0xFF, 0xFF, 0x3F, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x1F, 0xFF,
+0xFF, 0xFE, 0xF0, 0x80, 0x03, 0x07, 0x07, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F,
+0x9F, 0xFE, 0xFE, 0xFC, 0x70, 0x80, 0xF0, 0xFE, 0xFF, 0xFF, 0x1F, 0x03, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x07, 0x3F, 0xFF, 0xFE, 0xF0, 0xC0, 0x00, 0x00, 0x07, 0x07, 0x07, 0x07, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x01, 0x03, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x03, 0x01, 0x06, 0x07, 0x07,
+0x07, 0x01, 0x00, 0x00, 
+};
 
 OpticalReceiver phototransistor;  // create an instance of the receiver
-
 
 void setup()
 {
   Serial.begin(9600);                 // start the serial port on the Arduino
-  Serial.print("Size of union-packet: ");
-  Serial.println(sizeof(union packet));  
-  phototransistor.set_speed(laserreceivespeed);             // laser receive speed - should be 500+ bits/second, nominal 2000 (=2KHz)
+  //Serial.print("Size of union-packet: "); Serial.println(sizeof(union packet)); // Debug: check size of array
+  phototransistor.set_speed(MODULATE_SPEED);             // laser receive speed - should be 500+ bits/second, nominal 2000 (=2KHz)
   phototransistor.set_rxpin(PHOTOTRANSISTOR_RECEIVE);       // pin the phototransistor is connected to
   phototransistor.set_inverted(true);                       // if receive signal is inverted (Laser on = logic 0) set this to true
   phototransistor.begin();                                  // initialize the receiver
 
-  LCDInit(); //Init the LCD
-  LCDString("INITIALIZING DISPLAY");
-  delay(1700);
+  LCDInit(); //Start the LCD
+  /* Cosmetic frivolity on power-up. */
+  LCDBitmap(nasa_worm_BMP);
+  delay(1000);
+  LCDString("NASA SCaN Laser Relay Kit\n");
+  LCDString("Initializing display...");
+  delay(750);
+  LCDClear();
+  LCDString("Initial design by Tom Gitlin. Thanks, Tom!");
+  delay(200);
   LCDClear();
 }
 
-// Set up an interrupt service routine to receive characters
-// Arduino Timer2 reads the LIGHT_RECEIVE_PIN at laser receive speed to receive each half bit
+/* --------- INTERRUPT SERVICE ROUTINE (ISR) ----------
+   Set up an interrupt service routine to receive characters
+   Arduino Timer2 reads the LIGHT_RECEIVE_PIN at laser
+   receive speed to receive each half bit */
 ISR(TIMER2_COMPA_vect)
 {
   phototransistor.receive();   // check for a received character every timer interrupt
@@ -99,59 +107,58 @@ ISR(TIMER2_COMPA_vect)
 
 void loop()
 {
-
   displayChanged = false;
-  //Serial.print("T: \t");  Serial.print(pkt.temperature);
-  //Serial.print("\t H: \t"); Serial.println(pkt.humidity);
+  //Serial.print("T: \t");  Serial.print(pkt.temperature); Serial.print("\t H: \t"); Serial.println(pkt.humidity);
   //LCDString("WAITING FOR LASER");
-  // block here waiting for a byte to be received
-    byte received_byte;
-    bool good_data_read = phototransistor.GetByte(received_byte);
- 
-    if (good_data_read)
-    {
-      if (valid_read)
-      {
-          pkt.bytes[receive_count++] = received_byte;
-          Serial.print("Received: \t"); Serial.println(received_byte);
   
-          if (pkt.starter    == '\x01' &&
-              pkt.terminator == '\x04')
-          {
-              // COMPLETE PACKET HAS ARRIVED
-              Serial.print("Temp: \t");
-              Serial.println(pkt.temperature);
-              Serial.print("Humi: \t");
-              Serial.println(pkt.humidity);
-              LCDClear();
-              String foobar = "Temp: " + String(pkt.temperature) + '\n';
-              String foobar2 = "Humi: " + String(pkt.humidity);
-              LCDString(foobar.c_str());
-              LCDString(foobar2.c_str());
-              reset_packet();
-          }
-          else if (receive_count > sizeof(union packet) + 1)
-          {
-              reset_packet();
-              Serial.println("Packet's full; reset packet.");
-          }
-      }
-      else
+  /* block here waiting for a byte to be received */
+  byte received_byte;
+  bool good_data_read = phototransistor.GetByte(received_byte);
+
+  if (good_data_read)
+  {
+    if (valid_read)
+    {
+      pkt.bytes[receive_count++] = received_byte;
+      Serial.print("Received: \t"); Serial.println(received_byte);
+
+      if (pkt.starter    == '\x01' &&
+          pkt.terminator == '\x04')
       {
-          if (received_byte == '\x01')
-          {
-              Serial.println("Starting new packet. I'm excited!");
-              valid_read = true;
-              pkt.bytes[receive_count++] = received_byte;
-          }
-          //Serial.println("INVALID READ");
+        // COMPLETE PACKET HAS ARRIVED
+        Serial.print("Temp: \t"); Serial.println(pkt.temperature);
+        Serial.print("Humi: \t"); Serial.println(pkt.humidity);
+        LCDClear();
+        temperature_F = (pkt.temperature * 9/5) + 32; // Convert to awkward imperial units.
+        strTemper       = "Temp: " + String(pkt.temperature) + " `C" + '\n';
+        strTemperFahren = "      " + String(temperature_F) + " `F" + '\n';
+        strHumidi       = "Humi: " + String(pkt.humidity) + "%";
+        LCDString(strTemper.c_str());
+        LCDString(strHumidi.c_str());
+        reset_packet();
+      }
+      else if (receive_count > sizeof(union packet) + 1)
+      {
+        reset_packet();
+        Serial.println(" == Packet's full; reset packet. ==");
       }
     }
+    else
+    {
+      if (received_byte == '\x01')
+      // ASCII \x01 is the packet header.
+      {
+        Serial.println(" ==     Starting new packet.     ==");
+        valid_read = true;
+        pkt.bytes[receive_count++] = received_byte;
+      }
+    }
+  }
 } // end main loop
 
 void reset_packet()
 {
-    valid_read    = false;
-    receive_count = 0;
-    memset(&pkt, 0, sizeof(union packet));
+  valid_read    = false;
+  receive_count = 0;
+  memset(&pkt, 0, sizeof(union packet));
 }
--- OpticalTransmit_AM2320/OpticalTransmit_AM2320.ino
+++ OpticalTransmit_AM2320/OpticalTransmit_AM2320.ino
@@ -2,13 +2,12 @@
   Laser Transmitter Code
   
   This is a program using an AM2320 humidity and temperature sensor
-  using I2C to communicate to an Arduino.
+  using I2C to communicate to an Arduino. A laser is also connected 
+  to the Arduino to send data to a receiver.
   
   The sensor is connected to SCL -> SCL, SCA -> SCA, VCC -> 3-5V, GND -> GND.
   DON'T FORGET TO PULLUP YOUR SCL AND SDA LINES TO THE 5V RAIL WITH 2K-10K RESISTORS.
   
-  A laser is also connected to the Arduino to send data to a receiver.
-
   Hamming encoder/decoder and Optical modulator/demodulator are based on
   the LumenWire library written by Andrew Ramanjooloo and modified by
   Tom Gitlin.
@@ -20,60 +19,59 @@
 #include <Wire.h>             // library to communicate with the sensor using I2C
 #include <Adafruit_Sensor.h>  // generic sensor library 
 #include <Adafruit_AM2320.h>  // specific sensor library
-
-/*#define BME280_ADDRESS (0x76) // Need to set the I2C address to Low = 0x76 (vs High = 0x77)
-  */
-Adafruit_AM2320 am2320 = Adafruit_AM2320(); // create an I2C instance to talk to the sensor
-
 #include <HammingEncDec.h>    // include the Hamming encoder/decoder functionality
 #include <OpticalModDemod.h>  // include the modulator/demodulator functionality
 
-// new packet structure by Dan "Memes" Koris
+/* --------- PRE-SETUP ---------- */
+/* new packet structure by Dan "Memes" Koris - m241dan */
 typedef union packet
 {
-    char bytes[10];
-    struct 
-    {
-        byte  starter      = '\x01';
-        float temperature  = 0.0f;
-        float humidity     = 0.0f;
-        byte  terminator   = '\x04';
-    }; 
+  char bytes[10];
+  struct 
+  {
+    byte  starter      = '\x01';
+    float temperature  = 0.0f;
+    float humidity     = 0.0f;
+    byte  terminator   = '\x04';
+  }; 
 };
 
-OpticalTransmitter laser;     // create an instance of the transmitter
+Adafruit_AM2320 am2320    = Adafruit_AM2320(); // create an I2C instance to talk to the sensor
+OpticalTransmitter laser;         // create an instance of the transmitter
+const int CHAR_DELAY      = 30;   // delay between individual characters of a message (nominally 30ms)
+float temperature         = 0;
+float humidity            = 0; 
+static int PIN_LED_SENSOR = 5;    // Pin for an LED to indicate something is wrong with the sensor
+static int PIN_TX         = 13;   // Pin for the laser transmit wire
+static int MODULATE_SPEED = 2000; // laser modulation speed - should be 500+ bits/second, nominal 2000 (=2KHz)
+char incomingByte;                // variable to hold the byte to be encoded
+uint16_t msg;                     // variable to hold the message (character)
+unsigned long delaytime   = 500;  // delay between transmitting measurands in milliseconds (nominally 500ms)
+                                  // need a 'long' value due to it being compared to the Arduino millis value
 
-unsigned long delaytime = 500;  // delay between transmitting measurands in milliseconds (nominally 500ms)
-                              // need a 'long' value due to it being compared to the Arduino millis value
-const int CHAR_DELAY    = 30;   // delay between individual characters of a message (nominally 30ms)
-float temperature, humidity = 0; 
-boolean english=false;         // flag to use english units, otherwise use SI
-String strTemperature, strHumidity; // String values for the measurands
-char incomingByte;            // variable to hold the byte to be encoded
-uint16_t msg;                 // varible to hold the message (character)
-int LED_SensorError = 5;      // Pin for an LED to indicate something is wrong with the sensor
-int i;                        
-
+/* --------- ACTUAL SETUP ---------- */
 void setup() 
 {
-  pinMode(LED_SensorError, OUTPUT);
+  pinMode(PIN_LED_SENSOR, OUTPUT);
   Serial.begin(9600);
   am2320.begin();
   // Check for the sensor. If there is an error, blink the SensorError LED
-  while (!am2320.begin()) {  
-    //Serial.println(F("Could not find the temp/humidity/pressure sensor. Check the I2C address and/or wiring"));
-    digitalWrite(LED_SensorError, HIGH);
+  while (!am2320.begin()) 
+  {  
+    Serial.println(F("Could not find the temp/humidity/pressure sensor. Check the I2C address and/or wiring"));
+    digitalWrite(PIN_LED_SENSOR, HIGH);
     delay (500);
-    digitalWrite(LED_SensorError, LOW);
+    digitalWrite(PIN_LED_SENSOR, LOW);
     delay (500);
   }
-  laser.set_speed(2000);      // laser modulation speed - should be 500+ bits/second, nominal 2000 (=2KHz)
-  laser.set_txpin(13);        // pin the laser is connected to
+  laser.set_speed(MODULATE_SPEED);  
+  laser.set_txpin(PIN_TX);    
   laser.begin();              // initialize the laser
 
-} // END of setup();
+} /* END of setup(); */
 
-/* Set up an Interrupt Service Routine to transmit characters.
+/* --------- INTERRUPT SERVICE ROUTINE (ISR) ----------
+ *  Set up an Interrupt Service Routine to transmit characters.
  * Arduino Timer2 interrupt toggles the LIGHT_SEND_PIN at the 
  * laser send speed to transmit each half bit. */
 ISR(TIMER2_COMPA_vect)
@@ -81,30 +79,24 @@ ISR(TIMER2_COMPA_vect)
   laser.transmit(); // transmit a character if one is ready
 }
 
+/* --------- MAIN ---------- */
 void loop() 
 {
-    temperature = am2320.readTemperature();  // read the temperature from the sensor. [degrees C]
-    humidity    = am2320.readHumidity();     // read the humidity. Humidity is returned in percent relative humidity
+  temperature = am2320.readTemperature();  // read the temperature from the sensor. [degrees C]
+  humidity    = am2320.readHumidity();     // read the humidity. Humidity is returned in percent relative humidity
+ 
+  // Create our custom packet, load with data, and sandwich between terminators.
+  union packet pkt;
+  pkt.starter     = '\x01';
+  pkt.temperature = temperature;
+  pkt.humidity    = humidity;
+  pkt.terminator  = '\x04';
 
-    // if english units are desired, perform the conversion
-    if (english) 
-    {
-      temperature = temperature * 1.8 + 32.0; // convert C to F
-    }
+  // Send prepared packet.
+  laserTransmit(pkt);
+  delay(delaytime); 
     
-    union packet pkt;
-    pkt.starter     = '\x01';
-    pkt.temperature = 69; //temp debug static values 
-    pkt.humidity    = 420;
-    //pkt.temperature = temperature;
-    //pkt.humidity    = humidity;
-    pkt.terminator  = '\x04';
-    
-    laserTransmit(pkt);
-    delay(delaytime);
-     
-    
-} //END of loop()
+} /*END of loop() */
 
 void  laserTransmit(union packet pkt)
 {
@@ -114,4 +106,4 @@ void  laserTransmit(union packet pkt)
       laser.manchester_modulate(encoded_msg);       // modulate the character using the laser
       delay(CHAR_DELAY);
   }
-} // END of laserTransmit()
+} /* END of laserTransmit() */
