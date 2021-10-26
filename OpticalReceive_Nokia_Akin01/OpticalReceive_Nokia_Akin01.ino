33bd0b788ebfaf869c010fb923e6e85642ea6aa0
Jimmy Jimmy Jimmy <kestrel@gmail.com>
2021-10-25 21:06:53 -0400

    Putting """""""clean"""""""" sketches in to start from the ""ground"" up

---
+++ OpticalReceive_Nokia_Akin01/OpticalReceive_Nokia_Akin01.ino
@@ -0,0 +1,64 @@
+
+
+
+//#include "Nokia_LCD_functions.h"  //include library to drive NOKIA display
+#include <HammingEncDec.h>        // include the Hamming encoder/decoder functionality
+#include <OpticalModDemod.h>      // include the modulator/demodulator functionality
+OpticalReceiver phototransistor;  // create an instance of the receiver
+byte c; //holds byte returned from receiver
+
+unsigned long  timelastchar;          // variable to hold the time the last valid character was received
+bool           blankedvalues;         // flag to determine if values have been blanked for a bad laser link
+String         parameterValue;      // holds the measurand being built up character-by-character
+String         strTemperature, strPressure, strHumidity; // holds the values of the measurands
+
+void setup() 
+{
+  Serial.begin(9600);                 // start the serial port on the Arduino
+  Serial.println("NASA SCaN Gitlinkit Laser Relay demonstration -- powering on.");
+  phototransistor.set_speed(2000);             // laser receive speed - should be 500+ bits/second, nominal 2000 (=2KHz)
+  phototransistor.set_rxpin(2);       // pin the phototransistor is connected to
+  phototransistor.set_inverted(true);                       // if receive signal is inverted (Laser on = logic 0) set this to true
+  phototransistor.begin();                                  // initialize the receiver
+
+}
+
+/* --------- INTERRUPT SERVICE ROUTINE (ISR) ----------
+   Set up an interrupt service routine to receive characters
+   Arduino Timer2 reads the LIGHT_RECEIVE_PIN at laser
+   receive speed to receive each half bit */
+ISR(TIMER2_COMPA_vect)
+{
+  phototransistor.receive();   // check for a received character every timer interrupt
+}
+
+void loop() 
+{
+  phototransistor.GetByte(c);     // get a character from the laser receiver if one is available
+  
+  if (c>0)
+  {
+   // if a character is ready, look at it
+   Serial.println(c);
+    switch (c)
+    {       
+      // if the character is a terminator, store what was built in a variable and display it
+      case 84:         // ASCII T termination character for temperature, use string built to this point for temp
+        strTemperature=parameterValue;
+        parameterValue="";
+        Serial.println(strTemperature);
+        break;
+      case 80:        // ASCII P termination character for pressure, use string built to this point for pressure
+        strPressure=parameterValue;
+        parameterValue="";
+        break;    
+      case 72:        // ASCII H termination character for humidity, use string built to this point for humidity
+        strHumidity=parameterValue;
+        parameterValue="";
+        Serial.println(strHumidity);
+        break;
+      default :
+        parameterValue=parameterValue+=(char)c;  // keep building a string character-by-character until a terminator is found
+    }
+  } //end IFCHECK-ELSE (link is bad)
+} // end main loop
---
+++ OpticalTransmit_Akin01/OpticalTransmit_Akin01.ino
@@ -0,0 +1,73 @@
+#include <Wire.h>             // library to communicate with the sensor using I2C
+#include <Adafruit_Sensor.h>  // generic sensor library 
+#include <Adafruit_AM2320.h>  // specific sensor library
+
+/*#define BME280_ADDRESS (0x76) // Need to set the I2C address to Low = 0x76 (vs High = 0x77)
+  */
+Adafruit_AM2320 am2320 = Adafruit_AM2320(); // create an I2C instance to talk to the sensor
+
+#include <HammingEncDec.h>    // include the Hamming encoder/decoder functionality
+#include <OpticalModDemod.h>  // include the modulator/demodulator functionality
+
+OpticalTransmitter laser;     // create an instance of the transmitter
+
+unsigned long delaytime = 500;  // delay between transmitting measurands in milliseconds (nominally 500ms)
+                              // need a 'long' value due to it being compared to the Arduino millis value
+const int CHAR_DELAY    = 30;   // delay between individual characters of a message (nominally 30ms)
+float temperature, humidity = 0; 
+boolean english=false;         // flag to use english units, otherwise use SI
+String strTemperature, strHumidity; // String values for the measurands
+char incomingByte;            // variable to hold the byte to be encoded
+uint16_t msg;                 // varible to hold the message (character)
+int i;                        
+
+void setup() 
+{
+  Serial.begin(9600);
+  am2320.begin();
+  laser.set_speed(2000);      // laser modulation speed - should be 500+ bits/second, nominal 2000 (=2KHz)
+  laser.set_txpin(13);        // pin the laser is connected to
+  laser.begin();              // initialize the laser
+
+} // END of setup();
+
+/* Set up an Interrupt Service Routine to transmit characters.
+ * Arduino Timer2 interrupt toggles the LIGHT_SEND_PIN at the 
+ * laser send speed to transmit each half bit. */
+ISR(TIMER2_COMPA_vect)
+{
+  laser.transmit(); // transmit a character if one is ready
+}
+
+void loop() 
+{
+    temperature = am2320.readTemperature();  // read the temperature from the sensor. [degrees C]
+    humidity    = am2320.readHumidity();     // read the humidity. Humidity is returned in percent relative humidity
+
+    // if english units are desired, perform the conversion
+    if (english) 
+    {
+      temperature = temperature * 1.8 + 32.0; // convert C to F
+    }
+ 
+    strTemperature=String(temperature)+="T";
+    laserTransmit(strTemperature);
+    delay(delaytime);
+     
+    strHumidity=String(humidity)+="H";
+    laserTransmit(strHumidity);
+    delay(delaytime);
+
+} //END of loop()
+
+void  laserTransmit(String xmitmsg)
+{
+   for (i=0; i<(xmitmsg.length()+1); i++)   // transmit the string byte by byte
+   {
+      incomingByte=xmitmsg.charAt(i);       // get the character at position i
+      //Serial.print(incomingByte);
+      msg = hamming_byte_encoder(incomingByte); // encode the character
+      laser.manchester_modulate(msg);       // modulate the character using the laser
+      delay(CHAR_DELAY);                    // wait delay between transmitting individual characters of the message
+    }
+} // END of laserTransmit()
