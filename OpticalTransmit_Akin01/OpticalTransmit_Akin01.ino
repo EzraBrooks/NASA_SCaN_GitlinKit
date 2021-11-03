68535b189b7ee9d654f44dff434668e55c0d2f14
Jimmy Jimmy Jimmy <kestrel@gmail.com>
2021-11-02 20:48:41 -0400

    Cosmetic; indentation fixes.

--- OpticalTransmit_Akin01/OpticalTransmit_Akin01.ino
+++ OpticalTransmit_Akin01/OpticalTransmit_Akin01.ino
@@ -10,16 +10,16 @@ Adafruit_AM2320 am2320 = Adafruit_AM2320(); // create an I2C instance to talk to
 OpticalTransmitter laser;     // create an instance of the transmitter
 
 unsigned long       delaytime     = 500;  // delay between transmitting measurands in milliseconds (nominally 500ms)
-                                          // need a 'long' value due to it being compared to the Arduino millis value
+// need a 'long' value due to it being compared to the Arduino millis value
 const int           CHAR_DELAY    = 30;   // delay between individual characters of a message (nominally 30ms)
 float               temperatureC, temperatureF, humidity = 0;      // Float values for the measurands
 String              strTemperatureC, strTemperatureF, strHumidity; // String values for the measurands
 char                incomingByte;                                  // variable to hold the byte to be encoded
 uint16_t            msg;                                           // variable to hold the message (character)
-const uint8_t       PIN_LASER_XMIT = 13;                           //Pin for the laser transmitter                 
+const uint8_t       PIN_LASER_XMIT = 13;                           //Pin for the laser transmitter
 bool                sensor_good = false;  //Variable to check if the AAM2320 sensor is reading properly.
 
-void setup() 
+void setup()
 {
   Serial.begin(9600);
   Serial.println("NASA SCaN Gitlinkit Laser Relay demonstration -- powering on TRANSMITTER.");
@@ -30,7 +30,7 @@ void setup()
   temperatureC = am2320.readTemperature();
   //If the AM2320 isn't set up properly, then float temperatureC will read as NaN (not a number) and won't resolve as == itself
   if (temperatureC == temperatureC)
-  {  
+  {
     Serial.print("AM2320 sensor has been set up properly! Initial measurement is temperature: "); Serial.print(am2320.readTemperature()); Serial.println(" C.");
   }
   else
@@ -40,49 +40,49 @@ void setup()
 } // END of setup();
 
 /* Set up an Interrupt Service Routine to transmit characters.
- * Arduino Timer2 interrupt toggles the LIGHT_SEND_PIN at the 
- * laser send speed to transmit each half bit. */
+   Arduino Timer2 interrupt toggles the LIGHT_SEND_PIN at the
+   laser send speed to transmit each half bit. */
 ISR(TIMER2_COMPA_vect)
 {
   laser.transmit(); // transmit a character if one is ready
 }
 
-void loop() 
+void loop()
 {
-    temperatureC = am2320.readTemperature();  // read the temperature from the sensor. [degrees C]
-    temperatureF = temperatureC * 1.8 + 32.0; // convert C to F
-    humidity     = am2320.readHumidity();     // read the humidity. Humidity is returned in percent relative humidity
+  temperatureC = am2320.readTemperature();  // read the temperature from the sensor. [degrees C]
+  temperatureF = temperatureC * 1.8 + 32.0; // convert C to F
+  humidity     = am2320.readHumidity();     // read the humidity. Humidity is returned in percent relative humidity
 
-   //If the AM2320 isn't set up properly, then float temperatureC will read as NaN (not a number) and won't resolve as == itself
-   if (!(temperatureC == temperatureC))
-   {  
-     Serial.println("Not reading the AM2320 sensor; check the wiring and assembly instructions carefully.");
-   }
-   
-    strTemperatureC=String(temperatureC)+="T";
-    Serial.print("*= Temp: "); Serial.print(strTemperatureC); Serial.println(" °C =*");
-    laserTransmit(strTemperatureC);
-    delay(delaytime);
-    strTemperatureF=String(temperatureF)+="F";
-    Serial.print("*~ Temp: "); Serial.print(strTemperatureF); Serial.println(" °F ~*");
-    laserTransmit(strTemperatureF);
-    delay(delaytime);
-    
-    strHumidity=String(humidity)+="H";
-    Serial.print("*- Hum:  "); Serial.print(strHumidity); Serial.println("%  -*");
-    laserTransmit(strHumidity);
-    delay(delaytime);
+  //If the AM2320 isn't set up properly, then float temperatureC will read as NaN (not a number) and won't resolve as == itself
+  if (!(temperatureC == temperatureC))
+  {
+    Serial.println("Not reading the AM2320 sensor; check the wiring and assembly instructions carefully.");
+  }
+
+  strTemperatureC = String(temperatureC) += "T";
+  Serial.print("*= Temp: "); Serial.print(strTemperatureC); Serial.println(" °C =*");
+  laserTransmit(strTemperatureC);
+  delay(delaytime);
+  strTemperatureF = String(temperatureF) += "F";
+  Serial.print("*~ Temp: "); Serial.print(strTemperatureF); Serial.println(" °F ~*");
+  laserTransmit(strTemperatureF);
+  delay(delaytime);
+
+  strHumidity = String(humidity) += "H";
+  Serial.print("*- Hum:  "); Serial.print(strHumidity); Serial.println("%  -*");
+  laserTransmit(strHumidity);
+  delay(delaytime);
 
 } //END of loop()
 
 void  laserTransmit(String xmitmsg)
 {
-   for (int i=0; i<(xmitmsg.length()+1); i++)   // transmit the string byte by byte
-   {
-      incomingByte=xmitmsg.charAt(i);       // get the character at position i
-      //Serial.print(incomingByte);
-      msg = hamming_byte_encoder(incomingByte); // encode the character
-      laser.manchester_modulate(msg);       // modulate the character using the laser
-      delay(CHAR_DELAY);                    // wait delay between transmitting individual characters of the message
-    }
+  for (int i = 0; i < (xmitmsg.length() + 1); i++) // transmit the string byte by byte
+  {
+    incomingByte = xmitmsg.charAt(i);     // get the character at position i
+    //Serial.print(incomingByte);
+    msg = hamming_byte_encoder(incomingByte); // encode the character
+    laser.manchester_modulate(msg);       // modulate the character using the laser
+    delay(CHAR_DELAY);                    // wait delay between transmitting individual characters of the message
+  }
 } // END of laserTransmit()
